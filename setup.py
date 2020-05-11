7fd13fa0ddc86fe947cc680a0bffd725badcced7
Pedro Algarvio <pedro@algarvio.me>
2020-05-11 08:29:20 +0100

    Add poetry 1.0.5

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_poetry_dummy_package',
     version='0.0.0',
-    install_requires=['poetry==1.0.2'],
+    install_requires=['poetry==1.0.5'],
 )
